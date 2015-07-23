de9a06a1b0e0f6c22a759c6bc6e3fee9ff1c2a7a
wf <cmqilu@stumail.nwu.edu.cn>
2015-07-23 10:48:38 +0800

    add try again button
    add try again button

--- from.php
+++ from.php
@@ -14,13 +14,16 @@
 
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta http-equiv=”Content-Type” content=”text/html; charset=utf-8″>
+	<head>    
+		<meta http-equiv=”Content-Type” content=“text/html; charset=utf-8″>
 		<title>确认验证码</title>
 	</head>
 	<body>
 		<form method="post" action="./form.php">
-			<p>验证码图片：<img border="1" src="./captcha.php?r=<?php echo rand();?>" border="1" width="100" ></p>
+			<p>验证码图片：
+			<img id="captcha_img" border="1" src="./captcha.php?r=<?php echo rand();?>" border="1" width="100" >
+			<a href="javascript:void(0)" onclick="document.getElementById('captcha_img').src='./captcha.php?r='+Math.random()">换一个</a>		
+			</p>
 			<p>请输入图片中的内容:
 				<input type="text" name="authcode" value="" />
 			</p>
