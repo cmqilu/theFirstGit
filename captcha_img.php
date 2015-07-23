c3d9e01c50e81bf56db4ddd803044c9e7f90941f
wf <cmqilu@stumail.nwu.edu.cn>
2015-07-23 15:28:43 +0800

    创建图片验证码
    创建图片验证码

---
+++ captcha_img.php
@@ -0,0 +1,25 @@
+<?php
+	
+	header("content-type:text/html;charset=UTF-8");
+	session_start();
+	
+	$table = array(
+		'pic0' => '水母',
+		'pic1' => '考拉',
+		'pic2' => '企鹅',
+		'pic3' => '花',
+	);
+	
+	$index = rand( 0, 3);
+	
+	$value = $table['pic'.$index];
+
+	$_SESSION['authcode'] = $value;
+	
+	$filename = dirname(__FILE__).'\\pic'.$index.'.jpg';
+	$contents = file_get_contents( $filename );
+	
+
+	
+	echo $contents;
+	
